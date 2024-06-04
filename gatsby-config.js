abb8156b2a5c732a44563c94aff8ae14a5de6311
GitFaolan <faolanbrazil@gmail.com>
2024-06-04 01:14:57 +0100

    Update project with the latest changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,73 +3,75 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Faolán Brazil',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Faolán Brazil | UX Researcher / Interaction Designer`,
     // Description that goes under your name in main bio
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    // author: `@rfitzio`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    // github: `https://github.com/ryanfitzgerald`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/faolanb/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `I am a driven and creative individual with a diverse skill set and broad experience across multiple fields. I have seasoned practical experience in Product program management and my academic background in HCI and UX is robust. I have a profound interest in gaming, technology, and a good ol’ story. I believe these elements can all become catalysts for positive change. My commitment lies in mastering new technologies, tackling intricate challenges, and employing both logical reasoning and innovative thinking to enhance user experiences and foster growth.
+
+    Passionate about cutting-edge technologies like AI, Mixed Reality, and Social Networking, I advocate for using technology to enhance quality of life and create bold opportunities. With a track record of successfully leading teams and bringing ambitious ideas to fruition, I rely on my creativity and storytelling passion to drive innovation and make a meaningful impact on the world.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
-        name: 'Devfolio',
+        name: 'Can Virtual Reality Encourage Empathy for Autism?',
         description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
+          'lorem ipsum',
         link: 'https://github.com/RyanFitzgerald/devfolio',
       },
       {
-        name: 'ChromeExtensionKit',
+        name: 'Multimodal device',
         description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
+        'lorem ipsum',
         link: 'https://chromeextensionkit.com/?ref=devfolio',
       },
       {
-        name: 'Another Cool Project',
+        name: 'Whale Research',
         description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
+          'Lorem ipsum',
         link: 'https://github.com/RyanFitzgerald/devfolio',
       },
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
+        name: 'F. Brazil Reinforcements',
+        description: 'IT Systems Assistant Manager, July 2023 - Present',
         link: 'https://github.com/RyanFitzgerald/devfolio',
       },
       {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
+        name: 'Pocket Worlds',
+        description: 'Jr. Product Manager, January 2022 - October 2022',
         link: 'https://github.com/RyanFitzgerald/devfolio',
       },
       {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
+        name: 'Pocket Worlds',
+        description: 'Quality Assurance, May 2021 - January 2022',
         link: 'https://github.com/RyanFitzgerald/devfolio',
       },
     ],
     // Optional: List your skills, they must have `name` and `description`.
     skills: [
       {
-        name: 'Languages & Frameworks',
+        name: 'UX Research',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+          'Data Analysis, SPSS, Ethnography, User Persona, Surveys, Focus Groups, Observational Studies, Diary Entries, Affinity Diagramming',
       },
       {
-        name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
+        name: 'Computer Science',
+        description: 'Python, HTML, CSS, JavaScript, C#, Swift, SQL, Numpy, Pandas, r, Decision Trees, OOP, Unity Game Engine, Hardware & Software Acumen',
       },
       {
         name: 'Other',
         description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+          'AGILE Methodology, SAAS, eCommerce, B2B, B2C, AI, XR, UX Design, UX Writing, System Design, User Empathy',
       },
     ],
   },
